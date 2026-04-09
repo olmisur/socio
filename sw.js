afc7a2dcc4fbaa1520c2d14e681e18109490ff82
Olmisur <olmisur@gmail.com>
2026-04-10 00:26:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,18 @@
-const CACHE = 'socio-v2';
-const ASSETS = [
-  '/socio/',
-  '/socio/index.html',
-  '/socio/manifest.json',
-  '/socio/icons/icon-192.png',
-  '/socio/icons/icon-512.png'
-];
-
+const CACHE = 'socio-v3';
+ 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
-
+ 
 self.addEventListener('fetch', e => {
-  if (e.request.url.includes('api.anthropic.com') || e.request.url.includes('fonts.googleapis.com')) {
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      if (res.ok && e.request.method === 'GET') {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match('/socio/index.html')))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
+ 
